a973d995e966314dad98c20271e69d203ebd1cfd
burgokmen <burgokmen@gmail.com>
2023-10-20 00:45:19 +0300

    courses server test

---
+++ src/main/java/com/brutech/courseApi/controller/CourseController.java
@@ -0,0 +1,15 @@
+package com.brutech.courseApi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+    @GetMapping("/")
+    public String getAllCourses(){
+        return "All Courses";
+    }
+
+}
